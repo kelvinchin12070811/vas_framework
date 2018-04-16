6e852fd8d19a6bf55dc92be627af24679f36e705
Kelvin Chin <chinkaiwen@yahoo.com>
2018-04-16 14:40:11 +0800

    Added ColourPresets

--- vas_framework/src/libraries/vasframework/sdlcore/math/Colour.cpp
+++ vas_framework/src/libraries/vasframework/sdlcore/math/Colour.cpp
@@ -35,4 +35,24 @@ namespace sdl
 		SDL_Colour instance = { red, green, bule, alpha };
 		return instance;
 	}
+
+	const Colour ColourPresets::red = Colour(0xff, 0x00, 0x00);
+	const Colour ColourPresets::green = Colour(0x00, 0xff, 0x00);
+	const Colour ColourPresets::blue = Colour(0x00, 0x00, 0xff);
+	const Colour ColourPresets::black = Colour(0x00, 0x00, 0x00);
+	const Colour ColourPresets::white = Colour(0xff, 0xff, 0xff);
+	const Colour ColourPresets::pink = Colour(0xff, 0x96, 0xca);
+	const Colour ColourPresets::hotPink = Colour(0xff, 0x69, 0xb4);
+	const Colour ColourPresets::yellow = Colour(0xff, 0xff, 0x00);
+	const Colour ColourPresets::purple = Colour(0x80, 0x00, 0x80);
+	const Colour ColourPresets::indigo = Colour(0x4b, 0x00, 0x82);
+	const Colour ColourPresets::brown = Colour(0xa5, 0x2a, 0x2a);
+	const Colour ColourPresets::chocolate = Colour(0xd2, 0x69, 0x1e);
+	const Colour ColourPresets::orange = Colour(0xff, 0xa5, 0x00);
+	const Colour ColourPresets::crimson = Colour(0xdc, 0x14, 0x3c);
+	const Colour ColourPresets::grey = Colour(0x80, 0x80, 0x80);
+	const Colour ColourPresets::maroon = Colour(0x80, 0x00, 0x00);
+	const Colour ColourPresets::cyan = Colour(0x00, 0xff, 0xff);
+	const Colour ColourPresets::magenta = Colour(0xff, 0x00, 0xff);
+	const Colour ColourPresets::cc5004b7 = Colour(0xcc, 0x50, 0x04, 0xb7);
 }
--- vas_framework/src/libraries/vasframework/sdlcore/math/Colour.hpp
+++ vas_framework/src/libraries/vasframework/sdlcore/math/Colour.hpp
@@ -23,5 +23,28 @@ namespace sdl
 		uint8_t alpha{ 255 };
 	};
 
+	struct ColourPresets
+	{
+		static const Colour red;
+		static const Colour green;
+		static const Colour blue;
+		static const Colour black;
+		static const Colour white;
+		static const Colour pink;
+		static const Colour hotPink;
+		static const Colour yellow;
+		static const Colour purple;
+		static const Colour indigo;
+		static const Colour brown;
+		static const Colour chocolate;
+		static const Colour orange;
+		static const Colour crimson;
+		static const Colour grey;
+		static const Colour maroon;
+		static const Colour cyan;
+		static const Colour magenta;
+		static const Colour cc5004b7;
+	};
 	using Color = Colour;
+	using ColorPresets = ColourPresets;
 }
