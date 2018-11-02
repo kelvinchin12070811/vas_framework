f29c8940b643efc4d150672636d4fa7a78adf409
Kelvin Chin <chinkaiwen@yahoo.com>
2018-11-02 21:39:39 +0800

    Add some docs to attrwrap

--- vas_framework/src/vasframework/graphics/animation/AttrWrap.hpp
+++ vas_framework/src/vasframework/graphics/animation/AttrWrap.hpp
@@ -3,9 +3,14 @@
 
 namespace vas
 {
+	/** @addtogroup animate
+		  @{
+	*/
+	/** @brief The wraper of the numeric attribute of object.
+	*/
 	template <typename AttrType>
 	struct AttrWrap
-	{
+	{ /** @} */
 		using WrappedType = AttrType;
 		AttrWrap(std::function<void(AttrType)> mutator) : mutator(mutator) {}
 		void operator()(double value)
